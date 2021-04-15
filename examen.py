91155dc808ecfea97b9dd3d92dfbea09b40b25be
Ptmlol <39383908+Ptmlol@users.noreply.github.com>
2021-04-15 16:43:18 +0300

    facut part 1 din proiect
    part 1 done

--- examen.py
+++ examen.py
@@ -5,74 +5,110 @@ f = open("grades_summed.in", "r")
 g = open("initial_grades.out", "w")
 
 
-def rotate(l, n):
-    return l[n:] + l[:n]
+def rotate(li, n):
+    return li[n:] + li[:n]
 
 
-try:
-    file_content = f.read()
-    lines = file_content.strip().split("\n")
-    n = lines.pop(0)
-    n = int(n)
-    nested_sums = []
-    sums = []
+def calculate(n=None, sums=None):
+    # g.seek(0)
+    # g.truncate() daca vrem sa stergem continutul din fisier la fiecare test
+    if n is None and sums is None:
+        n, sums = define()
     solutions = [-1] * n
-    for grade in lines:
-        nested_sums.append(grade.split())
-    for i in range(0, len(nested_sums[0])):
-        sums.append(int(nested_sums[0][i]))
-except Exception as e:
-    print("Error occurred parsing the file:", e)
-    sys.exit(0)
+    if not 4 <= n <= 100000:
+        g.write(str(-1))
+        return -1
+    try:# pentru a putea scrie None in teste
+        if n != len(sums):
+            g.write(str(-1))
+            return -1
+    except TypeError:
+        g.write(str(-1))
+        return -1
+    n_sum = 0
+    for s_grade in sums:
+        n_sum += int(s_grade)  # suma totala a sumelor celor 2 vecini
+    n_sum = n_sum // 2  # suma notelor initiale
+    s = n_sum  # copie suma note initiale
 
-n_sum = 0
-t1 = 0
-t2 = 0
-for grade in sums:
-    n_sum += int(grade)
-n_sum = n_sum // 2
-s = n_sum
-if n % 4 == 0:
-    g.write(str(-1))
+    if n % 4 == 0:
+        g.write(str(-1))
+        return -1
 
-if n % 4 == 1:
-    for i in range(0, n-1):
-        if i % 4 == 0 or i % 4 == 1:
-            s -= sums[i]
-    solutions[n-1] = s
-    i = n-1
-    while i-3 >= 0:
-        solutions[i-3] = sums[i] - solutions[i]
-        solutions[i-1] = sums[i-3] - solutions[i-3]
-        i -= 1
+    if n % 4 == 1:
+        for i in range(0, n-1):
+            if i % 4 == 0 or i % 4 == 1:
+                s -= sums[i]
+        solutions[n - 1] = s
+        i = n-1
+        while i-3 >= 0:
+            solutions[i - 3] = sums[i] - solutions[i]
+            solutions[i - 1] = sums[i - 3] - solutions[i - 3]
+            i -= 1
+        for initial_grade in solutions:
+            if not -1000000000 <= initial_grade <= 1000000000:
+                g.write(str(-1))
+                return -1
+        solutions = rotate(solutions, 1)
+        g.write(str(solutions))
+        return solutions
 
-    g.write(str(rotate(solutions, 1)))
-    sys.exit()
+    if n % 4 == 3:
+        for i in range(0, n - 1):
+            if (i % 4 == 0 or i % 4 == 1) and n-i >= 4:
+                s -= sums[i]
+            elif n-i < 4 and i % 4 == 0:
+                s -= sums[i]
+        solutions[n - 2] = s
+        solutions[0] = sums[n - 2] - solutions[n - 2]
+        for i in range(2, n, 2):
+            solutions[i] = sums[i - 2] - solutions[i - 2]
+        solutions[1] = sums[n - 1] - solutions[n - 1]
+        for i in range(3, n, 2):
+            solutions[i] = sums[i - 2] - solutions[i - 2]
 
-if n % 4 == 3:
-    for i in range(0, n - 1):
-        if (i % 4 == 0 or i % 4 == 1) and n-i >= 4:
-            s -= sums[i]
-        elif n-i < 4 and i % 4 == 0:
-            s -= sums[i]
-    solutions[n - 2] = s
-    solutions[0] = sums[n-2] - solutions[n-2]
-    for i in range(2, n, 2):
-        solutions[i] = sums[i-2] - solutions[i-2]
-    solutions[1] = sums[n-1] - solutions[n-1]
-    for i in range(3, n, 2):
-        solutions[i] = sums[i - 2] - solutions[i - 2]
-    g.write(str(rotate(solutions, 1)))
-    sys.exit()
+        for initial_grade in solutions:
+            if not -1000000000 <= initial_grade <= 1000000000:
+                g.write(str(-1))
+                return -1
+        solutions = rotate(solutions, 1)
+        g.write(str(solutions))
+        return solutions
 
-if n % 4 == 2:
-    for i in range(0, n, 6):
-        solutions[i+5] = (sums[i+5] + sums[i+3] - sums[i+1])//2
-        solutions[i+4] = (sums[i+4] + sums[i+2] - sums[i])//2
-        solutions[i+1] = sums[i+5] - solutions[i+5]
-        solutions[i+3] = sums[i+1] - solutions[i+1]
-        solutions[i] = sums[i+4] - solutions[i+4]
-        solutions[i+2] = sums[i] - solutions[i]
-    g.write(str(rotate(solutions, 1)))
-    sys.exit()
+    if n % 4 == 2:
+        for i in range(0, n, 6):
+            solutions[i + 5] = (sums[i + 5] + sums[i + 3] - sums[i + 1]) // 2
+            solutions[i + 4] = (sums[i + 4] + sums[i + 2] - sums[i]) // 2
+            solutions[i + 1] = sums[i + 5] - solutions[i + 5]
+            solutions[i + 3] = sums[i + 1] - solutions[i + 1]
+            solutions[i] = sums[i + 4] - solutions[i + 4]
+            solutions[i + 2] = sums[i] - solutions[i]
+        for initial_grade in solutions:
+            if not -1000000000 <= initial_grade <= 1000000000:
+                g.write(str(-1))
+                return -1
+        solutions = rotate(solutions, 1)
+        g.write(str(solutions))
+        return solutions
 
+
+def define():
+    try:
+        file_content = f.read()
+        lines = file_content.strip().split("\n")
+        nr = lines.pop(0)
+        nr = int(nr)
+        nested_sums = []
+        g_sums = []
+        for grade in lines:
+            nested_sums.append(grade.split())
+        for i in range(0, len(nested_sums[0])):
+            g_sums.append(int(nested_sums[0][i]))
+        return nr, g_sums
+    except Exception as e:
+        print("Error occurred parsing the file:", e)
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    calculate()
---
+++ test.py
@@ -0,0 +1,87 @@
+import unittest
+
+import examen
+
+
+class TestEquivalencePartitioning(unittest.TestCase):
+
+    def test_calculate(self):# equivalence partitioning
+        result = examen.calculate(5, [6, 13, 11, 10, 10])
+        self.assertEqual(result, [4, 5, 9, 6, 1])
+
+        result = examen.calculate(5, [1000000929, -10000006, 23123120, 232323111, -9282818220])
+        self.assertEqual(result, -1)
+
+        result = examen.calculate(5, [1000000929, 10000006, 23123120, 232323111, 928281822])
+        self.assertEqual(result, -1)
+
+        result = examen.calculate(5, [1, 2, 3, 4])
+        self.assertEqual(result, -1)
+
+        result = examen.calculate(1, None)
+        self.assertEqual(result, -1)
+
+        result = examen.calculate(100050, None)
+        self.assertEqual(result, -1)
+
+
+class BoundaryValueAnalysis(unittest.TestCase):
+
+    def test_calculate(self):
+        result = examen.calculate(3, None)
+        self.assertEqual(result, -1)
+        result = examen.calculate(100001, None)
+        self.assertEqual(result, -1)
+        result = examen.calculate(4, None) # boundry admisibil dar ecuatie neadmisibila
+        self.assertEqual(result, -1)
+        result = examen.calculate(5, [5, 10, 12, 11, 11]) # boundry admisibil, ecuatie admisibila
+        self.assertEqual(result, [2, 2, 8, 9, 3])
+        result = examen.calculate(4, [-5000, -100000, -5000, -100000]) # boundry admisibi, ecuatie neadmisibila N11 + S11
+        self.assertEqual(result, -1)
+        result = examen.calculate(5, [509950000, -900010000, 10950000, 599990000, -999000000])  # boundry admisibi, ecuatie admisibila N11+S11 logic
+        self.assertEqual(result, [-1000000000, 9950000, 99990000, 1000000, 500000000])
+        result = examen.calculate(4, [-5000, 1999900000, -5000, 1999900000])  # boundry admisibi, ecuatie neadmisibila N11 + S12
+        self.assertEqual(result, -1)
+        result = examen.calculate(5, [509950000, 1099990000, 10950000, 599990000, 1001000000])  # boundry admisibi, ecuatie admisibila N11 + S12
+        self.assertEqual(result, [1000000000, 9950000, 99990000, 1000000, 500000000])
+        result = examen.calculate(4, [-5000, -900010001, -5000, -900010001])  # boundry admisibi, ecuatie neadmisibila N11 + S21
+        self.assertEqual(result, -1)
+        result = examen.calculate(5, [509950000, -900010001, 10950000, 599990000, -999000001])  # boundry admisibi, ecuatie admisibila N11 + S21
+        self.assertEqual(result, -1)
+        result = examen.calculate(4, [-5000, 1999900001, -5000, 1999900001])  # boundry admisibi, ecuatie neadmisibila N11 + S22
+        self.assertEqual(result, -1)
+        result = examen.calculate(5, [509950000, 1099990001, 10950000, 599990000, 1001000001])  # boundry admisibi, ecuatie admisibila N11 + S22
+        self.assertEqual(result, -1)
+        result = examen.calculate(100000, [-5000, -100000, -5000, -100000])  # boundry admisibi, ecuatie neadmisibila N12 + S11
+        self.assertEqual(result, -1)
+        result = examen.calculate(100000, [-5000, 1999900000, -5000, 1999900000])  # boundry admisibi, ecuatie neadmisibila N12 + S12
+        self.assertEqual(result, -1)
+        result = examen.calculate(100000, [-5000, -900010001, -5000, -900010001])  # boundry admisibi, ecuatie neadmisibila N12 + S21
+        self.assertEqual(result, -1)
+        result = examen.calculate(99999, [509950000, -900010001, 10950000, 599990000, -999000001])  # boundry admisibi, ecuatie admisibila N12 + S21
+        self.assertEqual(result, -1)
+        result = examen.calculate(100000, [-5000, 1999900001, -5000, 1999900001])  # boundry admisibi, ecuatie neadmisibila N12 + S22
+        self.assertEqual(result, -1)
+        result = examen.calculate(99999, [509950000, 1099990001, 10950000, 599990000, 1001000001])  # boundry admisibi, ecuatie admisibila N12 + S22
+        self.assertEqual(result, -1)
+
+
+class CategoryPartitioning(unittest.TestCase):
+    def test_calculate(self):
+        result = examen.calculate(-6, None)# n<3
+        self.assertEqual(result, -1)
+        result = examen.calculate(3, None)# n=3
+        self.assertEqual(result, -1)
+        result = examen.calculate(4, None)  # n=4
+        self.assertEqual(result, -1)
+        result = examen.calculate(5, [6, 13, 11, 10, 11])  # n=5, len(s) = n
+        self.assertEqual(result, [5, 4, 8, 6, 2])
+        result = examen.calculate(5, [6, 13, 11, 10])  # n=5, len(s) != n
+        self.assertEqual(result, -1)
+        result = examen.calculate(99999, None)  # n=99999 Caz exceptional (nu se poate testa) la fel si pentru n=100000
+        self.assertEqual(result, -1)
+        result = examen.calculate(100001, None)  # n=100001
+        self.assertEqual(result, -1)
+        result = examen.calculate(100005, None)  # n>100001
+        self.assertEqual(result, -1)
+
